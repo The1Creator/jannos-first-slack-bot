cab4314cc5b909a2e0a09cf40c5ba716465aafa0
The1Creator <janno@heathmont.net>
2019-04-01 10:21:02 +0300

    empty program

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const request = require('request');
 
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+    const reply = {
+        "status": "ok"
+    };
+    res.json(reply);
+});
+
 const listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
